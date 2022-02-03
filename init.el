ba49a03f87354316b569e225f36958f631088183
lqx <lqx@wooduan.com>
2022-02-03 23:53:01 +0800

    change settings to org and this is a fresh new settings

---
+++ init.el
@@ -0,0 +1,12 @@
+;;; init.el --- autoinsert                -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Copyright (C) 2022  liuliancao
+
+;;; Author: liuliancao <liuliancao@gmail.com>
+;;; Keywords: elisp
+;;; Code:
+(require 'org)
+(org-babel-load-file (expand-file-name "settings.org" user-emacs-directory))
+
+(provide 'init)
+;;; init.el ends here
